39c6fec5d570f25890979674f5ed43ec68d7c662
Jeff Chavez <jeff.chavez0828@gmail.com>
2023-01-23 21:07:58 +0800

    Update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+
+Users should be able to:
+By default the arrow is white: #fff
+When the user hovers over the hamburger, it turns pink: #f40082
+When the hamburger turns into an arrow, it turns yellow: #ffd200
+The arrow stays yellow (doesn’t turn pink), even if the user hovers over it.
+When the user clicks on the hamburger, it animates into an arrow.
+When the user clicks on the arrow, it animates into a hamburger
+*/
